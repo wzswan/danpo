1d59ebe4161f402c8785685e5052a1918fae1325
wzswan <wzswan@gmail.com>
2016-12-14 00:31:25 +0900

    RotatewithObject
    follow an object and rotating

---
+++ RotateDown.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateDown : MonoBehaviour {
+
+    public Transform target;
+    public float orbitDistance = 1.0f;
+    public float orbitDegreesPerSec = 30.0f;
+    public Vector3 relativeDistance = Vector3.zero;
+
+    // Use this for initialization
+    void Start () {
+
+        if (target != null)
+        {
+            relativeDistance = transform.position - target.position;
+        }
+
+    }
+
+    void Orbit()
+    {
+        if (target != null)
+        {
+            transform.position = target.position + relativeDistance;
+            transform.RotateAround(target.position, Vector3.up, orbitDegreesPerSec * Time.deltaTime);
+
+            relativeDistance = transform.position - target.position;
+        }
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+
+        Orbit();
+
+    }
+}
